956e1c634a7eca7a28361f9aa29a93fbf619522d
antoine <ant.meresse@gmail.com>
2020-10-28 01:37:02 +0100

    feat : react router add to project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Navbar.css';
 
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
+
 function Navbar() {
     return (
         <nav className="navbar">
-            <ul>
-                <li className="navbar_item">About me</li>
-                <li className="navbar_item">My Projects</li>
-                <li className="navbar_item">Contact me</li>
-            </ul>
+                <ul>
+                    <li className="navbar_item">
+                        <Link to="/">About me</Link>
+                    </li>
+                    <li className="navbar_item">
+                        <Link to="/projects">My Projects</Link>
+                    </li>
+                    <li className="navbar_item">
+                        <Link to="/contact">Contact me</Link>
+                    </li>
+                </ul>
         </nav>
     )
 }

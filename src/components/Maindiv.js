65a3b606b6bf8cf4a684c3170f4ff14cf499e745
antoine <ant.meresse@gmail.com>
2020-10-28 12:55:49 +0100

    feat : Projects component

--- src/components/Maindiv.js
+++ src/components/Maindiv.js
@@ -3,13 +3,14 @@ import './Maindiv.css';
 
 import {Switch, Route} from "react-router-dom";
 import Aboutme from "./Aboutme";
+import Projects from "./Projects";
 
 function Maindiv() {
 	return (
 		<div className="maindiv">
 			<Switch>
               <Route path="/projects">
-                <p>Projects</p>
+                <Projects/>
               </Route>
               
               <Route path="/contact">
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+	return (
+		<div className="projects">
+			<h1>I'm a "Projects" Component</h1>
+		</div>
+	);
+}
+
+export default Projects;
